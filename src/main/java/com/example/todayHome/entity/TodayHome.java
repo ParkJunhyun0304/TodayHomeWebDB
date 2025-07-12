aa7931b898e00a0cdac0e7106b3f4807d3361eda
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-12 20:55:19 +0900

    [Fix] Getter 직접 생성

--- src/main/java/com/example/todayHome/entity/TodayHome.java
+++ src/main/java/com/example/todayHome/entity/TodayHome.java
@@ -4,15 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 
 public class TodayHome {
@@ -25,4 +23,69 @@ public class TodayHome {
     private int width; // 가로
     private int depth; //세로
     private int height; // 높이
+
+    public void setUpdate(String name, int price, int category, int width, int depth, int height) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.width = width;
+        this.depth = depth;
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
