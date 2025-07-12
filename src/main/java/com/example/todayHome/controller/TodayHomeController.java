a53c57aba63ba555b46bd3f3fa5d61f1306c2b9b
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-12 20:19:50 +0900

    [Feat] RestAPI 생성

--- src/main/java/com/example/todayHome/controller/TodayHomeController.java
+++ src/main/java/com/example/todayHome/controller/TodayHomeController.java
@@ -1,4 +1,36 @@
 package com.example.todayHome.controller;
 
+import com.example.todayHome.entity.TodayHome;
+import com.example.todayHome.service.TodayHomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class TodayHomeController {
+    @Autowired private TodayHomeService todayHomeService;
+
+    // Save
+    @PostMapping("/todayHome")
+    public TodayHome saveTodayHome(@RequestBody TodayHome todayHome) {
+        return todayHomeService.saveTodayHome(todayHome);
+    }
+
+    @GetMapping("/todayHome")
+    public List<TodayHome> getAllTodayHome() {
+        return todayHomeService.fetchTodayHome();
+    }
+
+    @PutMapping("/departments/{id}")
+    public TodayHome
+    updateTodayHome(@PathVariable int id, @RequestBody TodayHome todayHome) {
+        return todayHomeService.updateTodayHome(todayHome, id);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteTodayHome(@PathVariable int id) {
+        todayHomeService.deleteTodayHome(id);
+        return "Delete Success";
+    }
 }
