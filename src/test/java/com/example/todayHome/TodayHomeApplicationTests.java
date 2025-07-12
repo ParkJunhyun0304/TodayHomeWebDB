013b439232a9e4663a847397ad3e7556814047d2
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-12 20:13:47 +0900

    [Init] SpringBoot 초기 세팅

---
+++ src/test/java/com/example/todayHome/TodayHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todayHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
